57b83968e9d10926fab4824b75868f39b595f80c
MeetMaratha <meet.maratha6@gmail.com>
2024-10-10 22:47:35 -0300

    fix: fix issue of branch name not exist

--- backend/src/repoLoader.py
+++ backend/src/repoLoader.py
@@ -19,7 +19,6 @@ def getRepo(config: Configuration):
         repo = git.Repo.clone_from(
             config.repositoryUrl,
             repoPath,
-            branch="master",
             progress=Progress(),
             odbt=git.GitCmdObjectDB,
         )
@@ -33,3 +32,4 @@ def getRepo(config: Configuration):
 class Progress(git.remote.RemoteProgress):
     def update(self, op_code, cur_count, max_count=None, message=""):
         print(self._cur_line, end="\r")
+
