f32c249313e7c07d8501c61bba9cf7bde50ff030
Basit <basit@basits-mbp.home>
2024-10-16 22:53:15 -0300

    feat: add loader for API call response handling

--- MLbackend/static/main.js
+++ MLbackend/static/main.js
@@ -6,6 +6,10 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
     const accessToken = document.getElementById('access-token').value;
     const email = document.getElementById('email').value;
 
+    // Get the loader element
+    const loader = document.querySelector('.loader');
+    loader.style.display = 'block';
+
     // Send a POST request to the Flask backend
     fetch('/api/v1/smells', {
         method: 'POST',
@@ -19,17 +23,22 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
         }),
     })
     .then(response => {
+        loader.style.display = 'none'; // Hide loader 
         if (!response.ok) {
+            alert('Something went wrong!');
             throw new Error('Network response was not ok');
         }
         return response.json(); // Parse the JSON response
     })
     .then(data => {
-        // Alert the message received from the server
-        alert(data.status); 
+        // Store the data securely in session storage
+        sessionStorage.setItem('resultData', JSON.stringify(data.result));
+        // Redirect to the results page
+        //window.location.href = '/MLbackend/templates/results.html';
+        alert('Success!');
     })
     .catch(error => {
         console.error('Error:', error); 
-        alert('An error occurred: ' + error.message); // Optional: Show an alert for the error
+        alert('Something went wrong!'); // Optional: Show an alert for the error
     });
 });
