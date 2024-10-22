5874c37e96bc6012b8372efa93645447c0deb99b
MeetMaratha <meet.maratha6@gmail.com>
2024-10-22 17:53:38 -0300

    test(#14): Testing multiple batches generation.

--- MLbackend/tests/test_prRequest.py
+++ MLbackend/tests/test_prRequest.py
@@ -88,6 +88,81 @@ class TestPRRequest(unittest.TestCase):
 
         return None
 
+    @patch("src.graphqlAnalysis.graphqlAnalysisHelper.runGraphqlRequest")
+    def test_prsAvailableTwoOfBatches(self, mock_runGraphqlRequest) -> None:
+        mock_runGraphqlRequest.return_value = {
+            "repository": {
+                "pullRequests": {
+                    "pageInfo": {
+                        "endCursor": "Y3Vyc29yOnYyOpHOBYEJRz==",
+                        "hasNextPage": False,
+                    },
+                    "nodes": [
+                        {
+                            "number": 42,
+                            "createdAt": "2024-09-22T09:00:00Z",
+                            "closedAt": "2024-09-25T12:30:00Z",
+                            "participants": {
+                                "nodes": [
+                                    {"login": "contributor1"},
+                                    {"login": "reviewer1"},
+                                ]
+                            },
+                            "commits": {"totalCount": 5},
+                            "comments": {
+                                "nodes": [
+                                    {"bodyText": "This looks great! Let's merge it."},
+                                    {
+                                        "bodyText": "Please address the requested changes."
+                                    },
+                                ]
+                            },
+                        },
+                        {
+                            "number": 57,
+                            "createdAt": "2024-10-22T09:00:00Z",
+                            "closedAt": None,
+                            "participants": {
+                                "nodes": [
+                                    {"login": "contributor1"},
+                                    {"login": "reviewer1"},
+                                ]
+                            },
+                            "commits": {"totalCount": 5},
+                            "comments": {
+                                "nodes": [
+                                    {"bodyText": "This looks great! Let's merge it."},
+                                    {
+                                        "bodyText": "Please address the requested changes."
+                                    },
+                                ]
+                            },
+                        },
+                    ],
+                }
+            }
+        }
+        batch_dates: List[datetime] = [
+            datetime.now(timezone.utc) - relativedelta(months=1, days=5),
+            datetime.now(timezone.utc) - relativedelta(days=5),
+        ]
+
+        result = prRequest(
+            pat="test_pat",
+            owner="test_owner",
+            name="test_name",
+            delta=relativedelta(months=+1),
+            batchDates=batch_dates,
+            logger=self.mock_logger,
+        )
+
+        self.assertEqual(len(result), 2)
+
+        mock_runGraphqlRequest.assert_called_once()
+        self.mock_logger.assert_not_called()
+
+        return None
+
 
 if __name__ == "__main__":
     unittest.main()
