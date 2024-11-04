65ded6358eb6206bd0b2386ad764f6ad55e33754
MeetMaratha <meet.maratha6@gmail.com>
2024-11-04 10:49:05 -0400

    feat(#14): handled test 2 issue 2 batch case

--- MLbackend/tests/test_issueRequest.py
+++ MLbackend/tests/test_issueRequest.py
@@ -84,6 +84,75 @@ class TestIssueRequest(unittest.TestCase):
 
         return None
 
+    @patch("src.graphqlAnalysis.graphqlAnalysisHelper.runGraphqlRequest")
+    def test_issuesAvailableTwoOfBatches(self, mock_runGraphqlRequest) -> None:
+        created_at_date = datetime.now() - relativedelta(months=1, days=10)
+        closed_at_date = datetime.now() - relativedelta(months=1, days=15)
+        mock_runGraphqlRequest.return_value = {
+            "repository": {
+                "issues": {
+                    "pageInfo": {
+                        "endCursor": "Y3Vyc29yOnYyOpHOBYEJRz==",
+                        "hasNextPage": False,
+                    },
+                    "nodes": [
+                        {
+                            "number": 57,
+                            "createdAt": (
+                                created_at_date + relativedelta(months=1, days=10)
+                            ).isoformat()
+                            + "Z",
+                            "closedAt": None,
+                            "participants": {
+                                "nodes": [{"login": "user1"}, {"login": "user2"}]
+                            },
+                            "comments": {
+                                "nodes": [
+                                    {"bodyText": "This is a comment on the issue."}
+                                ]
+                            },
+                        },
+                        {
+                            "number": 1,
+                            "createdAt": (
+                                created_at_date + relativedelta(days=10)
+                            ).isoformat()
+                            + "Z",
+                            "closedAt": closed_at_date.isoformat() + "Z",
+                            "participants": {
+                                "nodes": [{"login": "user1"}, {"login": "user2"}]
+                            },
+                            "comments": {
+                                "nodes": [
+                                    {"bodyText": "This is a comment on the issue."}
+                                ]
+                            },
+                        },
+                    ],
+                }
+            }
+        }
+        batch_dates: List[datetime] = [
+            datetime.now(timezone.utc) - relativedelta(months=1, days=5),
+            datetime.now(timezone.utc) - relativedelta(days=5),
+        ]
+
+        result = issueRequest(
+            pat="test_pat",
+            owner="test_owner",
+            name="test_name",
+            delta=relativedelta(months=+1),
+            batch_dates=batch_dates,
+            logger=self.mock_logger,
+        )
+
+        self.assertEqual(len(result), 2)
+
+        mock_runGraphqlRequest.assert_called_once()
+        self.mock_logger.assert_not_called()
+
+        return None
+
 
 if __name__ == "__main__":
     unittest.main()
