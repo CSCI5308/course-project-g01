afa9e389f924ba8aa37adbdc40054c004e1508c8
BasitTramboo <basit.tramboo@gmail.com>
2024-11-08 09:31:01 -0400

    fix: update import path

--- MLbackend/tests/test_validations.py
+++ MLbackend/tests/test_validations.py
@@ -1,15 +1,8 @@
 import os
 import sys
-
 import pytest
-
 from MLbackend.validations import (InvalidInputError, validate_email,
                                    validate_pat, validate_url)
-
-# Add the project directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
-
-
 def test_validate_url():
     with pytest.raises(InvalidInputError):
         validate_url("invalid_url")
