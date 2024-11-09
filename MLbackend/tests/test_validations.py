4322c6901609f684d95ac2153e2bc6e3992f0cc9
MeetMaratha <meet.maratha6@gmail.com>
2024-11-08 20:36:56 -0400

    chore: removed unnecessary imports

--- MLbackend/tests/test_validations.py
+++ MLbackend/tests/test_validations.py
@@ -1,13 +1,7 @@
-import os
-import sys
-
 import pytest
 
 from MLbackend.validations import (InvalidInputError, validate_email,
                                    validate_pat, validate_url)
-
-# Add the project directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 
 def test_validate_url():
