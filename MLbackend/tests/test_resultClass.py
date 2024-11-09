6e93b4e911105034cb66a43ff285dee7bb8c1585
MeetMaratha <meet.maratha6@gmail.com>
2024-11-09 02:07:53 -0400

    test(#58): Added test for checking type of value passed for authour count

--- MLbackend/tests/test_resultClass.py
+++ MLbackend/tests/test_resultClass.py
@@ -661,3 +661,34 @@ def test_addAuthorCountFailsDueToLessBatchSize(
     )
 
     return None
+
+
+@pytest.mark.parametrize(
+    "batch_dates, author_counts",
+    [
+        ([datetime.now(), datetime.now()], [5, "a"]),
+        ([datetime.now(), datetime.now(), datetime.now()], [5, "b", 6]),
+    ],
+)
+def test_addAuthorCountFailsDueToIncorrectAuthorValueType(
+    result_instance: Result,
+    batch_dates: List[datetime],
+    author_counts: List[Any],
+) -> None:
+
+    result_instance.logger.error.return_value = "Incorrect value type for author count"
+    result_instance.logger.info.return_value = "All values of Result are being reset"
+    result_instance.addBatchDates(batch_dates)
+
+    with pytest.raises(ValueError):
+        for idx, author_count in enumerate(author_counts):
+            result_instance.addAuthorCount(batch_idx=idx, author_count=author_count)
+
+    result_instance.logger.info.assert_called_once_with(
+        "All values of Result are being reset"
+    )
+    result_instance.logger.error.assert_called_once_with(
+        "Incorrect value type for author count"
+    )
+
+    return None
