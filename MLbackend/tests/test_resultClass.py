70eea8690e0516177cf88575ea7dd80f021775cc
MeetMaratha <meet.maratha6@gmail.com>
2024-11-09 02:05:09 -0400

    test(#58): Added test for checking for any inconsistency in length of batches and author counts length

--- MLbackend/tests/test_resultClass.py
+++ MLbackend/tests/test_resultClass.py
@@ -633,3 +633,31 @@ def test_addAuthorCountCorrect(
     )
 
     return None
+
+
+@pytest.mark.parametrize(
+    "batch_dates, author_counts",
+    [([datetime.now()], [5, 5]), ([datetime.now(), datetime.now()], [5, 1, 6])],
+)
+def test_addAuthorCountFailsDueToLessBatchSize(
+    result_instance: Result,
+    batch_dates: List[datetime],
+    author_counts: List[Any],
+) -> None:
+
+    result_instance.logger.error.return_value = f"Mismatch between batch size of {len(batch_dates)} and author counts of {len(batch_dates) + 1}"
+    result_instance.logger.info.return_value = "All values of Result are being reset"
+    result_instance.addBatchDates(batch_dates)
+
+    with pytest.raises(ValueError):
+        for idx, author_count in enumerate(author_counts):
+            result_instance.addAuthorCount(batch_idx=idx, author_count=author_count)
+
+    result_instance.logger.info.assert_called_once_with(
+        "All values of Result are being reset"
+    )
+    result_instance.logger.error.assert_called_once_with(
+        f"Mismatch between batch size of {len(batch_dates)} and author counts of {len(batch_dates) + 1}"
+    )
+
+    return None
