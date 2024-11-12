806b11bdebac2cf378ebc8a72e2716b9fe266a93
MeetMaratha <meet.maratha6@gmail.com>
2024-11-12 17:14:41 -0400

    feat(#60 #61): Added functionality to pass the new data to webapp for visualization. Updated PDF generation and sending code.

--- MLbackend/app.py
+++ MLbackend/app.py
@@ -1,14 +1,9 @@
 import os
 import traceback
 from pathlib import Path
-from typing import Any, List, Tuple
 
 from flask import Flask, jsonify, render_template, request, send_file
 from flask_mail import Mail, Message
-from reportlab.lib import colors
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.platypus import Paragraph, SimpleDocTemplate, Table, TableStyle
 
 from MLbackend.config import LOGGER
 from MLbackend.src.devNetwork import communitySmellsDetector
@@ -18,8 +13,8 @@ from MLbackend.validations import (InvalidInputError, validate_email,
 
 app = Flask(
     __name__,
-    template_folder="../frontend/templates/",
-    static_folder="../frontend/static/",
+    template_folder=os.path.join("..", "frontend", "templates"),
+    static_folder=os.path.join("..", "frontend", "static"),
 )
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
@@ -34,76 +29,6 @@ mail = Mail(app)
 @app.route("/")
 def home():
     return render_template("index.html")
-
-
-def generate_pdf1(
-    metrics_results, meta_results, smell_abbreviations, smells, PDF_FILE_PATH
-):
-    document = SimpleDocTemplate(PDF_FILE_PATH, pagesize=letter)
-    content = []
-
-    # Add title
-    styles = getSampleStyleSheet()
-    title_style = styles["Title"]
-    title = Paragraph("Community Smell Definitions and Metric Analysis", title_style)
-    content.append(title)
-
-    # Add community smell definitions
-    for smell_name in smell_abbreviations:
-        smell_definition = smells.get(smell_name)
-        if smell_definition:
-            definition = f"{smell_name}: {smell_definition}"
-            paragraph = Paragraph(definition, styles["Normal"])
-            content.append(paragraph)
-    # Add a page break after the definitions
-    content.append(Paragraph("<br/>", styles["Normal"]))
-
-    # Add Commit Analysis section
-    commit_analysis_title = Paragraph("Commit Analysis:", styles["Heading2"])
-    content.append(commit_analysis_title)
-
-    # Commit Analysis Table
-    commit_analysis_table = Table(meta_results)
-    commit_analysis_table.setStyle(
-        TableStyle(
-            [
-                ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
-                ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
-                ("ALIGN", (0, 0), (-1, -1), "CENTER"),
-                ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),
-                ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
-                ("BACKGROUND", (0, 1), (-1, -1), colors.beige),
-                ("GRID", (0, 0), (-1, -1), 1, colors.black),
-            ]
-        )
-    )
-    content.append(commit_analysis_table)
-
-    # Add Metrics section
-    metrics_title = Paragraph(
-        "<br/><b>Commit and PR Analysis Metrics:</b>", styles["Heading2"]
-    )
-    content.append(metrics_title)
-
-    # Metrics Table
-    metrics_table = Table(metrics_results)
-    metrics_table.setStyle(
-        TableStyle(
-            [
-                ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
-                ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
-                ("ALIGN", (0, 0), (-1, -1), "CENTER"),
-                ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),
-                ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
-                ("BACKGROUND", (0, 1), (-1, -1), colors.beige),
-                ("GRID", (0, 0), (-1, -1), 1, colors.black),
-            ]
-        )
-    )
-    content.append(metrics_table)
-
-    # Build the PDF
-    document.build(content)
 
 
 output_path: Path = Path(".", "MLbackend", "src", "results")
@@ -123,8 +48,7 @@ def detect_smells():
         validate_url(url)
         validate_email(email)
         validate_pat(pat)
-        global result
-        result, df = communitySmellsDetector(
+        communitySmellsDetector(
             pat=pat,
             repo_url=url,
             senti_strength_path=senti_strength_path,
@@ -146,9 +70,7 @@ def detect_smells():
 
         send_email(
             email=email,
-            meta_results=result_ins.getMetaResults(),
-            metrics_results=result_ins.metric_datas,
-            smell_abbreviations=result_ins.smells,
+            pdf_file_path=result_ins.pdf_file_path,
         )
         return render_template("results.html", data=result_ins.getWebResult())
     except InvalidInputError as e:
@@ -160,30 +82,15 @@ def detect_smells():
 
 
 @app.route("/api/v1/pdf", methods=["GET"])
-def generate_pdf():
+def downloadPDF():
     try:
 
         metrics_results = result["metrics"]
         meta_results = result["meta"]
         smell_abbreviations = result["smell_results"][1:]
         PDF_FILE_PATH = os.path.join(output_path, "..", "smell_report.pdf")
-        smells = {
-            "OSE": "Organizational Silo Effect: Isolated subgroups lead to poor communication, wasted resources, and duplicated code.",
-            "BCE": "Black-cloud Effect: Information overload due to limited collaboration and a lack of experts, causing knowledge gaps.",
-            "PDE": "Prima-donnas Effect: Resistance to external input due to ineffective collaboration, hindering team synergy.",
-            "SV": "Sharing Villainy: Poor-quality information exchange results in outdated or incorrect knowledge being shared.",
-            "OS": "Organizational Skirmish: Misaligned expertise and communication affect productivity, timelines, and costs.",
-            "SD": "Solution Defiance: Conflicting technical opinions within subgroups cause delays and uncooperative behavior.",
-            "RS": "Radio Silence: Formal, rigid procedures delay decision-making and waste time, leading to project delays.",
-            "TFS": "Truck Factor Smell: Concentration of knowledge in few individuals leads to risks if they leave the project.",
-            "UI": "Unhealthy Interaction: Weak, slow communication among developers, with low participation and long response times.",
-            "TC": "Toxic Communication: Negative, hostile interactions among developers, resulting in frustration, stress, and potential project abandonment.",
-        }
-
         # Generate PDF
-        generate_pdf1(
-            metrics_results, meta_results, smell_abbreviations, smells, PDF_FILE_PATH
-        )
+        generate_pdf1(metrics_results, meta_results, smell_abbreviations, PDF_FILE_PATH)
 
         # Send the generated PDF to the frontend
         return send_file(PDF_FILE_PATH, as_attachment=True)
@@ -191,45 +98,22 @@ def generate_pdf():
         return jsonify({"error": str(e)}), 500
 
 
-def send_email(
-    email: str,
-    meta_results: List[List[List[Any]]] | List[List[Any]],
-    metrics_results: (
-        List[List[Tuple[str, int, float, float]]] | List[Tuple[str, int, float, float]]
-    ),
-    smell_abbreviations: List[List[str]] | List[str],
-) -> None:
-    PDF_FILE_PATH = os.path.join(output_path, "smell_report.pdf")
-    smells = {
-        "OSE": "Organizational Silo Effect: Isolated subgroups lead to poor communication, wasted resources, and duplicated code.",
-        "BCE": "Black-cloud Effect: Information overload due to limited collaboration and a lack of experts, causing knowledge gaps.",
-        "PDE": "Prima-donnas Effect: Resistance to external input due to ineffective collaboration, hindering team synergy.",
-        "SV": "Sharing Villainy: Poor-quality information exchange results in outdated or incorrect knowledge being shared.",
-        "OS": "Organizational Skirmish: Misaligned expertise and communication affect productivity, timelines, and costs.",
-        "SD": "Solution Defiance: Conflicting technical opinions within subgroups cause delays and uncooperative behavior.",
-        "RS": "Radio Silence: Formal, rigid procedures delay decision-making and waste time, leading to project delays.",
-        "TFS": "Truck Factor Smell: Concentration of knowledge in few individuals leads to risks if they leave the project.",
-        "UI": "Unhealthy Interaction: Weak, slow communication among developers, with low participation and long response times.",
-        "TC": "Toxic Communication: Negative, hostile interactions among developers, resulting in frustration, stress, and potential project abandonment.",
-    }
+def send_email(email: str, pdf_file_path: Path) -> None:
 
-    # We are taking smell_abbreviations[0] because the result class is handling single and multi batch working. But here we are just considering single batch process
-    generate_pdf1(
-        metrics_results[0],
-        meta_results,
-        smell_abbreviations[0],
-        smells,
-        PDF_FILE_PATH,
-    )
     msg = Message(
         subject="Community Smells Detector",
         sender="g01communitysmellsdetector@gmail.com",
         recipients=[email],
     )
     msg.body = "Hey, PFA smells report"
-    with app.open_resource(os.path.join("..", output_path, "smell_report.pdf")) as fp:
-        msg.attach("smell_report.pdf", "application/pdf", fp.read())
-    mail.send(msg)
+    try:
+        with open(pdf_file_path, "rb") as fp:
+            msg.attach("smell_report.pdf", "application/pdf", fp.read())
+        mail.send(msg)
+    except FileNotFoundError:
+        LOGGER.error("PDF file not found. Check if the file path is correct.")
+    except Exception as e:
+        LOGGER.error(f"An unexpected error occurred: {e}")
 
 
 if __name__ == "__main__":
