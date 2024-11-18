235964d1bcabd7aa40c902fbbf36bc018912a4e2
MeetMaratha <meet.maratha6@gmail.com>
2024-11-18 16:17:08 -0400

    test(#58): Added test for checking for any inconsistency in length of batches and pr comment toxicity percentage length

--- MLbackend/tests/test_resultClass.py
+++ MLbackend/tests/test_resultClass.py
@@ -2175,3 +2175,36 @@ def test_addPRCommentToxicityPercentageCorrect(
 
     return None
 
+
+@pytest.mark.parametrize(
+    "batch_dates, pr_comment_toxicity_percentages",
+    [([datetime.now()], [5, 5]), ([datetime.now(), datetime.now()], [5, 1, 6])],
+)
+def test_addPRCommentToxicityPercentageFailsDueToLessBatchSize(
+    result_instance: Result,
+    batch_dates: List[datetime],
+    pr_comment_toxicity_percentages: List[Any],
+) -> None:
+
+    result_instance.logger.error.return_value = f"Mismatch between batch size of {len(batch_dates)} and pr_comment_toxicity_percentage of {len(batch_dates) + 1}"
+    result_instance.logger.info.return_value = "All values of Result are being reset"
+    result_instance.addBatchDates(batch_dates)
+
+    with pytest.raises(ValueError):
+        for idx, pr_comment_toxicity_percentage in enumerate(
+            pr_comment_toxicity_percentages
+        ):
+            result_instance.addPRCommentToxicityPercentage(
+                batch_idx=idx,
+                pr_comment_toxicity_percentage=pr_comment_toxicity_percentage,
+            )
+
+    result_instance.logger.info.assert_called_once_with(
+        "All values of Result are being reset"
+    )
+    result_instance.logger.error.assert_called_once_with(
+        f"Mismatch between batch size of {len(batch_dates)} and pr_comment_toxicity_percentage of {len(batch_dates) + 1}"
+    )
+
+    return None
+
