93b3eb7b508ba1933fbda80dcc1a137d52ffc80a
MeetMaratha <meet.maratha6@gmail.com>
2024-11-18 15:47:46 -0400

    test(#58): Added test for handling incorrect value type being passed for pr comment sentiment negative count

--- MLbackend/tests/test_resultClass.py
+++ MLbackend/tests/test_resultClass.py
@@ -1970,3 +1970,40 @@ def test_addPRCommentSentimentNegativeCountFailsDueToLessBatchSize(
 
     return None
 
+
+@pytest.mark.parametrize(
+    "batch_dates, pr_comment_sentiment_negative_counts",
+    [
+        ([datetime.now(), datetime.now()], [5, "a"]),
+        ([datetime.now(), datetime.now(), datetime.now()], [5, "b", 6]),
+    ],
+)
+def test_addPRCommentSentimentNegativeCountFailsDueToIncorrectPRCommentSentimentNegativeValueType(
+    result_instance: Result,
+    batch_dates: List[datetime],
+    pr_comment_sentiment_negative_counts: List[Any],
+) -> None:
+
+    result_instance.logger.error.return_value = (
+        "Incorrect value type for pr_comment_sentiment_negative count"
+    )
+    result_instance.logger.info.return_value = "All values of Result are being reset"
+    result_instance.addBatchDates(batch_dates)
+
+    with pytest.raises(ValueError):
+        for idx, pr_comment_sentiment_negative_count in enumerate(
+            pr_comment_sentiment_negative_counts
+        ):
+            result_instance.addPRCommentSentimentNegativeCount(
+                batch_idx=idx,
+                pr_comment_sentiment_negative_count=pr_comment_sentiment_negative_count,
+            )
+
+    result_instance.logger.info.assert_called_once_with(
+        "All values of Result are being reset"
+    )
+    result_instance.logger.error.assert_called_once_with(
+        "Incorrect value type for pr_comment_sentiment_negative count"
+    )
+
+    return None
