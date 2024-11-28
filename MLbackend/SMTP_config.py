0885988a9b244f36fe1ead21ed8c5b3b6dedc6c4
Basit Tramboo <basit.tramboo@gmail.com>
2024-11-28 17:35:34 -0400

    Fixes: (#66) Fixes Security Hotspots

--- MLbackend/SMTP_config.py
+++ MLbackend/SMTP_config.py
@@ -1,7 +1,18 @@
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+email = os.getenv('EMAIL')
+password = os.getenv('PASSWORD')
+
+
 def configure_app(app):
     app.config["MAIL_SERVER"] = "smtp.gmail.com"
     app.config["MAIL_PORT"] = 587
-    app.config["MAIL_USERNAME"] = "g01communitysmellsdetector@gmail.com"
-    app.config["MAIL_PASSWORD"] = "gmki hznr hixx bihb"
+    app.config["MAIL_USERNAME"] = email
+    app.config["MAIL_PASSWORD"] = password
     app.config["MAIL_USE_TLS"] = True
     app.config["MAIL_USE_SSL"] = False
